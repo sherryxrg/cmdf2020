1acc74cfc842a1ff4eb227f60bd217416e00588f
Sherry <sherryxrg@outlook.com>
2020-03-07 13:31:45 -0800

    first commit with base project files

---
+++ hercard/hercard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  hercard
+//
+//  Created by Sherry Guo on 2020-03-07.
+//  Copyright © 2020 Sherry Guo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hercard/hercard/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hercard
+//
+//  Created by Sherry Guo on 2020-03-07.
+//  Copyright © 2020 Sherry Guo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
