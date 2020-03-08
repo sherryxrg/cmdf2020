1a39847467bb83848573d1f5ca458ad1631816a9
Yuni <yshuh0126@gmail.com>
2020-03-07 17:25:27 -0800

    Updated the search page

---
+++ hercard/hercard/SearchVCViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SearchVCViewController.swift
+//  hercard
+//
+//  Created by Yunseong hur on 2020-03-07.
+//  Copyright © 2020 Sherry Guo. All rights reserved.
+//
+
+import UIKit
+
+class SearchVCViewController: UIViewController {
+//    
+//    let items = ["AI architect","Business analyst", "Cloud architect", "Data", "Developer", "AI DevOps engineers", "Network security", "Systems administrator"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+//    func collectionView(collectionView: UICollectionView, numberOfItemsInsection section: Int)-> Int{
+//        return items.count
+//    }
+//
+//    func collectionView(collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)-> UICollectionViewCell{
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+//        cell.btn_AI_architect.setTitle(items[indexPath.item], for: .normal)
+//
+//        return cell
+//    }
+//
+//    func collectionView(collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+//        print(indexPath.item)
+//    }
+//
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
